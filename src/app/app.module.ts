dfcb2e34fcafec16e35d62ba2c3c6028ed8daabd
Leo <leonardosilva01107@gmail.com>
2024-12-03 12:09:43 -0300

    ajustes no nacbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BigCardComponent } from './components/big-card/big-card.component';
-
-import { MenuHomeComponent } from './components/menu-home/menu-home.component';
-import { MenuInfoComponent } from './components/menu-info/menu-info.component';
+import { MenuInfoComponent } from './components/header/menu-info/menu-info.component';
 import { ReservarComponent } from './components/reservar/reservar.component';
 import { CardapioComponent } from './pages/cardapio/cardapio.component';
 import { DaCasaComponent } from './pages/da-casa/da-casa.component';
@@ -15,10 +12,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { MaioresPedidosComponent } from './pages/maiores-pedidos/maiores-pedidos.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { SaboresComponent } from './pages/sabores/sabores.component';
-import { MenuBarComponent } from './components/menu/menu-bar/menu-bar.component';
-import { MenuLoginComponent } from './components/menu/menu-login/menu-login.component';
 import { MenuLocalComponent } from './components/menus/menu-local/menu-local.component';
-import { MenuLogoComponent } from './components/menus/menu-logo/menu-logo.component';
 import { MenuSocialComponent } from './components/menus/menu-social/menu-social.component';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -28,14 +22,15 @@ import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { CarrinhoPedidosComponent } from './components/carrinho-pedidos/carrinho-pedidos.component';
 import { MonteComponent } from './monte/monte.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BigCardComponent,
+    
     CadastroComponent,
     
-    MenuHomeComponent,
+    
     MenuInfoComponent,
     
     ReservarComponent,
@@ -46,15 +41,16 @@ import { MonteComponent } from './monte/monte.component';
     MaioresPedidosComponent,
     RodapeComponent,
     SaboresComponent,
-    MenuBarComponent,
-    MenuLoginComponent,
+    
+   
     MenuLocalComponent,
-    MenuLogoComponent,
+  
     MenuSocialComponent,
     LoginComponent,
     AcessoUsuarioComponent,
     CarrinhoPedidosComponent,
-    MonteComponent
+    MonteComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
