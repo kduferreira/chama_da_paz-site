5c7f146106a92d428c740813446f8696f3b5391a
Leo <leonardosilva01107@gmail.com>
2024-12-08 23:30:34 -0300

    ajuste

--- src/app/dataFake/itens-cardapio.ts
+++ src/app/dataFake/itens-cardapio.ts
@@ -6,52 +6,52 @@ export const dataFakeCategoria=[
     "itens": [
       {
         "idItem": 1,
-        "nome": "Coca-Cola",
-        "descricao": "coca gelada",
-        "preco": 8.00,
-        "id_categoria": 1
-      },
-      {
-        "idItem": 1,
-        "nome": "Coca-Cola",
-        "descricao": "coca gelada",
-        "preco": 8.00,
-        "id_categoria": 1
-      },
-      {
-        "idItem": 1,
-        "nome": "Coca-Cola",
-        "descricao": "coca gelada",
-        "preco": 8.00,
-        "id_categoria": 1
-      },
-      {
-        "idItem": 1,
-        "nome": "Coca-Cola",
-        "descricao": "coca gelada",
-        "preco": 8.00,
-        "id_categoria": 1
+        "nome": "Pizza Margherita",
+        "descricao": "Pizza clássica com molho de tomate, queijo mussarela e manjericão fresco.",
+        "preco": 30.00,
+        "id_categoria": 2
       },
       {
         "idItem": 2,
-        "nome": "Coca-Cola",
-        "descricao": "coca gelada",
-        "preco": 8.00,
-        "id_categoria": 1
+        "nome": "Pizza Pepperoni",
+        "descricao": "Pizza com queijo mussarela e fatias de pepperoni picante.",
+        "preco": 35.00,
+        "id_categoria": 2
       },
       {
-        "idItem": 2,
-        "nome": "Coca-Cola",
-        "descricao": "coca gelada",
-        "preco": 8.00,
-        "id_categoria": 1
+        "idItem": 3,
+        "nome": "Pizza Quatro Queijos",
+        "descricao": "Deliciosa mistura de mussarela, parmesão, gorgonzola e provolone.",
+        "preco": 40.00,
+        "id_categoria": 2
       },
       {
-        "idItem": 2,
-        "nome": "Coca-Cola",
-        "descricao": "coca gelada coca gelada geladageladageladageladacoca gelada coca gelada geladageladageladagelada coca gelada coca gelada geladageladageladageladacoca gelada coca gelada geladageladageladagelada",
-        "preco": 8.00,
-        "id_categoria": 1
+        "idItem": 4,
+        "nome": "Pizza Portuguesa",
+        "descricao": "Molho de tomate, mussarela, presunto, ovos, cebola e azeitonas.",
+        "preco": 38.00,
+        "id_categoria": 2
+      },
+      {
+        "idItem": 5,
+        "nome": "Pizza Calabresa",
+        "descricao": "Queijo mussarela e linguiça calabresa fatiada com cebolas.",
+        "preco": 32.00,
+        "id_categoria": 2
+      },
+      {
+        "idItem": 6,
+        "nome": "Pizza Vegana",
+        "descricao": "Massa integral, molho de tomate, queijo vegano, cogumelos e vegetais frescos.",
+        "preco": 42.00,
+        "id_categoria": 2
+      },
+      {
+        "idItem": 7,
+        "nome": "Pizza de Frango com Catupiry",
+        "descricao": "Frango desfiado temperado com catupiry e mussarela.",
+        "preco": 36.00,
+        "id_categoria": 2
       }
     ]
   },
